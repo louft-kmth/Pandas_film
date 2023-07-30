20130c64a1c6a9c3d2fb8d08e15f8c3c4a2cb3d7
SITRAKA RAJAONARIVELO <110409327+louft-kmth@users.noreply.github.com>
2023-07-30 17:29:15 +0200

    Update final_ml_projet_2_kung_fu_pandas.py

--- final_ml_projet_2_kung_fu_pandas.py
+++ final_ml_projet_2_kung_fu_pandas.py
@@ -17,8 +17,8 @@ pd.options.mode.chained_assignment = None
 from ast import literal_eval
 import streamlit as st
 import numpy as np
-# import matplotlib.pyplot as plt
-# import seaborn as sns
+import matplotlib.pyplot as plt
+import seaborn as sns
 
 from sklearn.preprocessing import StandardScaler
 
